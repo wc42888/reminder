737ea76982b69fc031ced25e6bdca70f0a0b3c6d
chao wang <chaowang@chaos-mbp.qantas.com.au>
2018-01-24 10:39:21 +1100

    updated navigation icon

--- src/navigators/index.js
+++ src/navigators/index.js
@@ -1,11 +1,29 @@
+import React from 'react';
 import { TabNavigator } from 'react-navigation';
+import { MaterialIcons } from '@expo/vector-icons';
 import { ReminderScreen, SettingsScreen } from '../screens';
 
 export default TabNavigator({
   Reminder: {
+    navigationOptions: {
+      tabBarIcon: () => (
+        <MaterialIcons
+          name="event-note"
+          size="24"
+        />
+      ),
+    },
     screen: ReminderScreen,
   },
   Settings: {
+    navigationOptions: {
+      tabBarIcon: () => (
+        <MaterialIcons
+          name="settings"
+          size="24"
+        />
+      ),
+    },
     screen: SettingsScreen,
   },
 });
